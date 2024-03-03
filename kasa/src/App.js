5dacc40187ba983d03fb55fd7cde372a57879ca0
anth-o <a.dumaine06670@gmail.com>
2024-03-03 20:58:39 +0100

    ajout REACT router + différentes pages

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import FicheLogement from "./pages/FicheLogement";
+import APropos from "./pages/Apropos";
+import Erreur from "./pages/Erreur";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/fiche-logement/:id" element={<FicheLogement />}></Route>
+          <Route path="/a-propos" element={<APropos />}></Route>
+          <Route path="*" element={<Erreur />}></Route>
+      </Routes>
     </div>
   );
 }
